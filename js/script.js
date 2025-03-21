e780ea8fd953b4e21061566d9b92e4849ad707af
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2025-03-20 21:33:24 -0400

    udt

--- js/script.js
+++ js/script.js
@@ -11,3 +11,18 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         errorMessage.textContent = "Usuario o contraseña incorrectos";
     }
 });
+
+//Botn Usuario
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+    
+    if (username === "admin" && password === "1234") {
+        alert("Bienvenido a Prestan2");
+        window.location.href = "portal.html";
+    } else {
+        errorMessage.textContent = "Usuario o contraseña incorrectos";
+    }
+});
