499c567cae35bab3f1f5af64e63f808b07687194
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2025-03-20 22:19:28 -0400

    Update firebase-config.js

--- Database/firebase-config.js
+++ Database/firebase-config.js
@@ -1,12 +1,12 @@
 // Configuración de Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyAoR5nIZScneP9wNnzbveqXFxcSMhdF4_U",
-    authDomain: "prestan2-6059e.firebaseapp.com",
-    databaseURL: "https://prestan2-6059e-default-rtdb.firebaseio.com",
-    projectId: "prestan2-6059e",
-    storageBucket: "prestan2-6059e.firebasestorage.app",
-    messagingSenderId: "758979429000",
-    appId: "1:758979429000:web:46133ec0e3a8bf628b553d"
+  apiKey: "AIzaSyAoR5nIZScneP9wNnzbveqXFxcSMhdF4_U",
+  authDomain: "prestan2-6059e.firebaseapp.com",
+  databaseURL: "https://prestan2-6059e-default-rtdb.firebaseio.com",
+  projectId: "prestan2-6059e",
+  storageBucket: "prestan2-6059e.firebasestorage.app",
+  messagingSenderId: "758979429000",
+  appId: "1:758979429000:web:46133ec0e3a8bf628b553d"
 };
 
 // Inicializar Firebase
