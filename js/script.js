792c40b7eebec06ed34fb319355cf7e04110d53d
Jeury Suarez Hernandez <49211071+jeuryblackrain@users.noreply.github.com>
2025-03-20 21:49:30 -0400

    upd

--- js/script.js
+++ js/script.js
@@ -11,3 +11,37 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
         errorMessage.textContent = "Usuario o contraseña incorrectos";
     }
 });
+
+function abrirFormulario() {
+    document.getElementById("userForm").style.display = "block";
+}
+
+function cerrarFormulario() {
+    document.getElementById("userForm").style.display = "none";
+}
+
+function guardarUsuario() {
+    const fullName = document.getElementById("fullName").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const phone = document.getElementById("phone").value;
+    const email = document.getElementById("email").value;
+    const registerDate = document.getElementById("registerDate").value;
+    
+    const newUserRef = db.ref("usuarios").push();
+    newUserRef.set({
+        nombre: fullName,
+        usuario: username,
+        contraseña: password,
+        telefono: phone,
+        email: email,
+        fechaRegistro: registerDate
+    }, (error) => {
+        if (error) {
+            alert("Error al guardar usuario");
+        } else {
+            alert("Usuario guardado exitosamente");
+            cerrarFormulario();
+        }
+    });
+}
